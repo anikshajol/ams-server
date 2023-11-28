3133d7737bfdd141ad792b3772f833c040c8d914
anikshajol <anikshajol@gmail.com>
2023-11-29 04:03:30 +0600

    post asset api create

--- index.js
+++ index.js
@@ -39,9 +39,45 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    app.post("/jwt", async (req, res) => {
+      try {
+        const user = req.body;
+        console.log(user, "user of token");
+
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+          expiresIn: "1h",
+        });
+
+        res.send({ token });
+      } catch (error) {}
+    });
+
+    // middlewares for verifytoken;
+
+    // const verifyToken = (req, res, next) => {
+    //   if (!req.headers.authorization) {
+    //     return res.status(401).send({ message: "unauthorized access" });
+    //   }
+    // };
+
+    const verifyToken = (req, res, next) => {
+      // console.log("inside verify token", req.headers.authorization);
+      if (!req.headers.authorization) {
+        res.status(401).send({ message: "Forbidden access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "Forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
     // assets list get
 
-    app.get("/assets", async (req, res) => {
+    app.get("/assets", verifyToken, async (req, res) => {
       try {
         const filter = req.query;
         const query = {
@@ -51,6 +87,14 @@ async function run() {
           },
         };
         const result = await assetsCollection.find(query).toArray();
+        res.send(result);
+      } catch (error) {}
+    });
+
+    app.post("/assets", verifyToken, async (req, res) => {
+      try {
+        const asset = req.body;
+        const result = await assetsCollection.insertOne(asset);
         res.send(result);
       } catch (error) {}
     });
@@ -131,10 +175,30 @@ async function run() {
     //   } catch (error) {}
     // });
 
-    app.get("/users/admin/:email", async (req, res) => {
+    // app.get("/users/employee/:email", async (req, res) => {
+    //   try {
+    //     const email = req.params.email;
+    //     const query = { email: email };
+
+    //     const user = await usersCollection.findOne(query);
+
+    //     let employee = false;
+    //     if (user) {
+    //       employee = user?.role === "employee";
+    //     }
+
+    //     res.send({ admin });
+    //   } catch (error) {}
+    // });
+
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
       try {
         const email = req.params.email;
         const query = { email: email };
+
+        if (email != req.decoded.email) {
+          return res.status(403).send({ message: "unauthorized access" });
+        }
 
         const user = await usersCollection.findOne(query);
 
