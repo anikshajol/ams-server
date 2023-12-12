6af1afe69eced47eb872b7c3ec2238c82acdb167
anikshajol <anikshajol@gmail.com>
2023-12-13 00:56:30 +0600

    done

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const packagesCollection = client.db("anifaAMS").collection("packages");
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     app.post("/jwt", async (req, res) => {
       try {
@@ -342,11 +342,11 @@ async function run() {
       });
     });
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
